41169d38798c563b55cc78611073b1d0960995da
angleyanalbedo <100198247+angleyanalbedo@users.noreply.github.com>
2025-11-02 09:49:15 +0800

    增加命令行参数

--- SMBCopy/Program.cs
+++ SMBCopy/Program.cs
@@ -12,11 +12,43 @@ namespace SMBCopy
         /// 应用程序的主入口点。
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Usage: Program <sourcePath> <destinationPath> <excludePatterns>");
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+
+            }
+            else
+            {
+                RunCommandLineMode(args);
+            }
+            
+        }
+
+        private static void RunCommandLineMode(string[] args)
+        {
+            string srcdir = args[0];
+            string dstdir = args[1];
+            var exclude = args[2].Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            try
+            {
+                // 调用文件复制方法
+                SMBFileCopy.CopyFilesFromSMB(srcdir, dstdir, exclude, progress =>
+                {
+                    Console.WriteLine($"Progress: {progress}%");
+                });
+
+                Console.WriteLine("文件复制完成！");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("发生错误: " + ex.Message);
+            }
         }
     }
 }
