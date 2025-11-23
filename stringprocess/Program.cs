5d1d2cfe21736619819f41e08b4b32809b1ee7dd
angleyanalbedo <100198247+angleyanalbedo@users.noreply.github.com>
2025-11-23 20:38:35 +0800

    update

---
+++ stringprocess/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace stringprocess
+{
+    public static List<List<int>> GetBraketRange(List<string> lists)
+    {
+        Stack<List<int>> stack = new Stack<List<int>>();
+        Dictionary<int,List<int>> dict = new Dictionary<int,List<int>>();
+        for(var line in lists)
+        {
+            if (line.Equals("("))
+            {
+                stack.Push(new List<int>());
+            }
+            
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
