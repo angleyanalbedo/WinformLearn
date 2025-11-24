fbd7d02bbad357fbefa4bdfa9d9c5bf8017aa5d5
angleyanalbedo <100198247+angleyanalbedo@users.noreply.github.com>
2025-11-24 15:34:13 +0800

    first init

--- stringprocess/Program.cs
+++ stringprocess/Program.cs
@@ -7,22 +7,43 @@ using System.Collections;
 
 namespace stringprocess
 {
-    public static List<List<int>> GetBraketRange(List<string> lists)
-    {
-        Stack<List<int>> stack = new Stack<List<int>>();
-        Dictionary<int,List<int>> dict = new Dictionary<int,List<int>>();
-        for(var line in lists)
-        {
-            if (line.Equals("("))
-            {
-                stack.Push(new List<int>());
-            }
-            
-        }
-    }
+    
 
     internal class Program
     {
+        public static List<List<int>> GetBraketRange(List<string> lists)
+        {
+            List<List<int>> res = new List<List<int>>();
+            int deep = -1;
+            foreach (string str in lists)
+            {
+                if (str.Trim().Equals("("))
+                {
+                    deep++;
+                    res.Add(new List<int>());
+                    res[deep].Add(lists.IndexOf(str));
+                }
+                else if (str.Trim().Equals(")") && deep == -1)
+                {
+                    return new List<List<int>>();
+                }
+                else if (str.Trim().Equals(")") && deep > -1)
+                {
+                    res[deep].Add(lists.IndexOf(str));
+                    deep--;
+
+                }
+                else if (deep > -1)
+                {
+                    res[deep].Add(lists.IndexOf(str));
+                }
+            }
+            if (deep > -1)
+            {
+                return new List<List<int>>();
+            }
+            return res;
+        }
         static void Main(string[] args)
         {
         }
